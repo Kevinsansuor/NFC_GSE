6620391b538aa2678b2399d0844b00b53fce68c9
Cri255 <cd.baronrodriguez@unicienciabga.edu.co>
2024-11-29 12:30:55 -0500

    Diseño de Interfaz Principal

--- nfc_flutter_proyect/lib/main.dart
+++ nfc_flutter_proyect/lib/main.dart
@@ -1,113 +1,107 @@
 import 'package:flutter/material.dart';
-import 'package:nfc_manager/nfc_manager.dart';
-import 'package:logger/logger.dart';
-
-var logger = Logger();
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart'; 
 void main() {
-  runApp(const MyApp());
+  runApp(const NFCApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class NFCApp extends StatelessWidget {
+  const NFCApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter NFC Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter NFC Demo'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _nfcData = 'Presiona el botón para leer NFC';
-  bool _isReading = false;
-
-  void _startNFCReading() async {
-    setState(() {
-      _isReading = true;
-      _nfcData = 'Esperando datos NFC...';
-    });
-
-    try {
-      bool isAvailable = await NfcManager.instance.isAvailable();
-      logger.d('isAvailable: $isAvailable');
-
-      if (isAvailable) {
-        NfcManager.instance.startSession(
-          //TODO: onDiscoveredSession
-          onDiscovered: (NfcTag tag) async {
-
-            String data = tag.data.toString();
-
-              setState(() {
-                _nfcData = data;
-              });
-              NfcManager.instance.stopSession();
-            } 
-        ); } else {
-        _showErrorSnackbar('NFC no está disponible en este dispositivo.');
-        setState(() {
-          _nfcData = 'NFC no disponible.';
-        });
-      }
-    } catch (e) {
-      logger.e('Error: ${e.toString()}');
-      _showErrorSnackbar('Error al leer NFC: ${e.toString()}');
-      setState(() {
-        _nfcData = 'Error al leer NFC.';
-      });
-    } finally {
-      setState(() {
-        _isReading = false;
-      });
-    }
-  }
-
-  void _showErrorSnackbar(String message) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        backgroundColor: Colors.red,
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.secondaryFixed,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            _nfcData,
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+               const SizedBox(height: 60),
+              // Título
+              Text(
+                'NFC App',
+                style: GoogleFonts.montserrat(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              // Descripción
+              Text(
+                'Bienvenido a NFC App, con esta aplicación podrás, escanear, añadir y configurar tarjetas con la tecnología NFC de tu dispositivo Android.',
+                style: GoogleFonts.montserrat(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+              // Espacio entre el primer párrafo y la línea de división
+              const SizedBox(height: 15),
+              const Divider(
+                color: Colors.white24,
+                thickness: 1,
+                height: 25,
+              ),
+              const SizedBox(height: 15), // Espacio adicional después de la línea de separación
+              // Subtítulo
+              Text(
+                'Prueba NFC',
+                style: GoogleFonts.montserrat(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              // Descripción del botón
+              Text(
+                'Verifiquemos que tu dispositivo tiene la tecnología NFC incorporada.',
+                style: GoogleFonts.montserrat(
+                  color: Colors.white70,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 90),
+              // Icono NFC personalizado usando flutter_vector_icons
+              Center(
+                child: Icon(
+                  MaterialCommunityIcons.nfc,  // Usando el icono de NFC de MaterialCommunityIcons
+                  size: 100,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 100),
+              
+              // Botón con bordes redondeados, sin relleno
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Acción al presionar el botón
+                  },
+                  style: ElevatedButton.styleFrom(
+                    side: const BorderSide(color: Colors.white, width: 2), // Borde blanco
+                    padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 16),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30), // Bordes redondeados
+                    ),
+                    backgroundColor: Colors.transparent, // Sin relleno
+                  ),
+                  child: Text(
+                    'Verificar NFC',
+                    style: GoogleFonts.montserrat(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: _isReading ? null : _startNFCReading,
-            child: Text(_isReading ? 'Leyendo...' : 'Leer NFC'),
-          ),
-        ],
+        ),
       ),
     );
   }
