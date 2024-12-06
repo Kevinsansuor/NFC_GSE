ddb0b08432abfd17c54768e3db3e3135c839f7a7
Cri255 <cd.baronrodriguez@unicienciabga.edu.co>
2024-12-06 13:37:45 -0500

    Diseño de paginas de verificacion y error

--- nfc_flutter_proyect/lib/pages/error_nfc/error_nfc_page.dart
+++ nfc_flutter_proyect/lib/pages/error_nfc/error_nfc_page.dart
@@ -1,39 +1,147 @@
 import 'package:flutter/material.dart';
-import 'package:nfc_flutter_proyect/widgets/body_text/body_text.dart';
-import 'package:nfc_flutter_proyect/widgets/h1_text/tittle_text.dart';
+import 'package:nfc_flutter_proyect/pages/verify_nfc/verify_nfc_page.dart';
+import 'package:nfc_flutter_proyect/widgets/h2_text/large_text.dart';
 
 class ErrorNfcPage extends StatelessWidget {
-  const ErrorNfcPage({super.key});
+  const ErrorNfcPage ({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Color primaryColor = Theme.of(context).colorScheme.primary;
+    final Color primaryOpaqueColor =
+        Theme.of(context).colorScheme.onPrimaryFixed;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Error NFC'),
-      ),
-      body: const SafeArea(
-        child: Center(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 100,
-                ),
-                SizedBox(height: 20),
-                TittleText(
-                  text: 'No se detectó NFC',
-                ),
-                BodyText(
-                  text:
-                      'Bienvenido a NFC App, con esta aplicación podrás, escanear, añadir y configurar tarjetas con la tecnología NFC de tu dispositivo Android.',
-                ),
-              ],
+      backgroundColor: const Color(0xFF1A1A1A), 
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0, bottom: 25),
+                    child: Column(
+                      textDirection: TextDirection.ltr,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(top: 70.0), 
+                            child: Text(
+                              'Verificacion Fallida',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge
+                                  ?.copyWith(
+                                    color: primaryColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2.5,
+                    width: double.infinity,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: primaryOpaqueColor,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25),
+                          child: Column(
+                            textDirection: TextDirection.ltr,
+                            children: [
+                              LargeText(
+                              text: 'No se encontro NFC',
+                              color: primaryColor,
+                              textStyle: Theme.of(context).textTheme.titleLarge,
+                            ),
+
+                              Text(
+                                'No se encuentra actividad NFC reconocida',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.copyWith(
+                                      color: primaryColor,
+                                      height: 1.0, 
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Row(
+                          children: [
+                            Flexible(
+                              fit: FlexFit.loose,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: Column(
+                                  children: [
+                                    Center(
+                                        child: Image(
+                                          image: AssetImage('assets/images/appImages/fail.png'),
+                                          width: 280, 
+                                          height: 280,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge,
+                                    side: BorderSide(
+                                        color: primaryOpaqueColor, width: 2.5),
+                                    padding: const EdgeInsets.all(25),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                  ),
+                                 onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => VerifyNfcPage(), 
+                                        ),
+                                      );
+                                    },
+                                  child: const Text('Intentar de nuevo'),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- nfc_flutter_proyect/lib/pages/valid_nfc/valid_nfc_page.dart
+++ nfc_flutter_proyect/lib/pages/valid_nfc/valid_nfc_page.dart
@@ -1,29 +1,141 @@
 import 'package:flutter/material.dart';
+import 'package:nfc_flutter_proyect/widgets/h2_text/large_text.dart';
 
 class ValidNfcPage extends StatelessWidget {
   const ValidNfcPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final Color primaryColor = Theme.of(context).colorScheme.primary;
+    final Color primaryOpaqueColor =
+        Theme.of(context).colorScheme.onPrimaryFixed;
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Success'),),
-      body: const SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Dispositivo con NFC',
-                style: TextStyle(fontSize: 24),
+      backgroundColor: const Color(0xFF1A1A1A), 
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 0, bottom: 25),
+                    child: Column(
+                      textDirection: TextDirection.ltr,
+                      children: [
+                        Padding(
+                            padding: const EdgeInsets.only(top: 70.0), 
+                            child: Text(
+                              'Verificacion Exitosa',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .displayLarge
+                                  ?.copyWith(
+                                    color: primaryColor,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                            ),
+                          ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 2.5,
+                    width: double.infinity,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: primaryOpaqueColor,
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 25),
+                          child: Column(
+                            textDirection: TextDirection.ltr,
+                            children: [
+                              LargeText(
+                              text: 'Dispositivo con NFC',
+                              color: primaryColor,
+                              textStyle: Theme.of(context).textTheme.titleLarge,
+                            ),
+
+                              Text(
+                                'Tu dispositivo cuenta con la tecnología necesaria para poder escanear, añadir y configurar tarjetas con NFC.',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .bodyLarge
+                                    ?.copyWith(
+                                      color: primaryColor,
+                                      height: 1.0, 
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Row(
+                          children: [
+                            Flexible(
+                              fit: FlexFit.loose,
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(vertical: 5),
+                                child: Column(
+                                  children: [
+                                    Center(
+                                        child: Image(
+                                          image: AssetImage('assets/images/appImages/successful.png'),
+                                          width: 280, 
+                                          height: 280,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 15),
+                                child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    textStyle: Theme.of(context)
+                                        .textTheme
+                                        .labelLarge,
+                                    side: BorderSide(
+                                        color: primaryOpaqueColor, width: 2.5),
+                                    padding: const EdgeInsets.all(25),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(50),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    //acciones del boton
+                                  },
+                                  child: const Text('Siguiente'),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
-              SizedBox(height: 20),
-              Icon(
-                Icons.check_circle,
-                color: Colors.green,
-                size: 64,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
